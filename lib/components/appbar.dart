cbb3f7208b105189548d05d981ba47444a461f09
NicolasDutronc <nicolas.dutronc@orange.fr>
2020-07-26 18:39:08 +0200

    feat(appbar): added a logout button on the app bar

--- lib/components/appbar.dart
+++ lib/components/appbar.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shoplist/users/authentication/bloc.dart';
+import 'package:shoplist/users/authentication/events.dart';
 
-AppBar getAppBar() {
+AppBar getAppBar(BuildContext context) {
   return AppBar(
     title: Text("Courses"),
+    actions: [
+      IconButton(
+        icon: const Icon(Icons.power_settings_new),
+        onPressed: () {
+          print('logout');
+          BlocProvider.of<AuthenticationBloc>(context).add(LoggedOut());
+        },
+      )
+    ],
   );
 }
