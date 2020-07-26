5dc6318464d4f7d7e6ef3265300308ed6303c69f
NicolasDutronc <nicolas.dutronc@orange.fr>
2020-07-26 18:50:26 +0200

    refact(memory users repo): added a delay to simulate the network call

--- lib/users/repository/memory.dart
+++ lib/users/repository/memory.dart
@@ -2,7 +2,8 @@ import 'package:shoplist/users/repository/repository.dart';
 
 class MemoryUserRepository extends UserRepository {
   @override
-  Future<String> authenticate({String username, String password}) {
-    return Future<String>.value("token");
+  Future<String> authenticate({String username, String password}) async {
+    await Future.delayed(Duration(seconds: 1));
+    return 'token';
   }
 }
