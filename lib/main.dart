a814438f4fd3dc11d3d57e979300763d4253f15c
NicolasDutronc <nicolas.dutronc@orange.fr>
2020-07-26 18:45:12 +0200

    feat(home): replaced memory repositories by real http repositories. Rendering of the home is conditionned by the authentication state the app is in

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,36 @@ import 'package:provider/provider.dart';
 import 'package:shoplist/lists/list/bloc.dart';
 import 'package:shoplist/lists/lists/bloc.dart';
 import 'package:shoplist/lists/lists/events.dart';
-import 'package:shoplist/lists/repository/memory.dart';
+import 'package:shoplist/lists/repository/http.dart';
 import 'package:shoplist/lists/repository/repository.dart';
 import 'package:shoplist/screens/home.dart';
 import 'package:shoplist/users/authentication/bloc.dart';
-import 'package:shoplist/users/repository/memory.dart';
+import 'package:shoplist/users/authentication/events.dart';
+import 'package:shoplist/users/authentication/states.dart';
+import 'package:shoplist/users/login/ui/login_page.dart';
+import 'package:shoplist/users/repository/http.dart';
 import 'package:shoplist/users/repository/repository.dart';
 
 void main() {
+  String backendUrl = '10.0.2.2:8080';
   BlocSupervisor.delegate = SimpleBlocDelegate();
   runApp(
     MultiProvider(
       providers: [
         RepositoryProvider<UserRepository>(
-          create: (context) => MemoryUserRepository(),
-        ),
-        RepositoryProvider<ListsRepository>(
-          create: (context) => MemoryListsRepository(),
+          create: (context) => HttpUserRepository(
+            url: backendUrl,
+          ),
         ),
         BlocProvider<AuthenticationBloc>(
           create: (context) => AuthenticationBloc(
+            userRepository: RepositoryProvider.of<UserRepository>(context),
+          )..add(AppStarted()),
+        ),
+        RepositoryProvider<ListsRepository>(
+          create: (context) => HttpListRepository(
+            url: backendUrl,
+            authenticationBloc: BlocProvider.of<AuthenticationBloc>(context),
             userRepository: RepositoryProvider.of<UserRepository>(context),
           ),
         ),
@@ -61,7 +71,27 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: Home("CouCourses"),
+      home: BlocBuilder<AuthenticationBloc, AuthenticateState>(
+        builder: (context, state) {
+          if (state is AuthenticationUninitialised) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (state is AuthenticationAuthenticated) {
+            return Home();
+          }
+          if (state is AuthenticationUnauthenticated) {
+            return LoginPage();
+          }
+          if (state is AuthenticationLoading) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return null;
+        },
+      ),
     );
   }
 }
@@ -69,19 +99,22 @@ class MyApp extends StatelessWidget {
 class SimpleBlocDelegate extends BlocDelegate {
   @override
   void onEvent(Bloc bloc, Object event) {
-    print('event : ' + event.toString());
+    print('bloc : ' + bloc.toString() + ' => event : ' + event.toString());
     super.onEvent(bloc, event);
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
-    print('transition : ' + transition.toString());
+    print('bloc : ' +
+        bloc.toString() +
+        ' => transition : ' +
+        transition.toString());
     super.onTransition(bloc, transition);
   }
 
   @override
   void onError(Bloc bloc, Object error, StackTrace stackTrace) {
-    print('error : ' + error.toString());
+    print('bloc : ' + bloc.toString() + ' => error : ' + error.toString());
     super.onError(bloc, error, stackTrace);
   }
 }
