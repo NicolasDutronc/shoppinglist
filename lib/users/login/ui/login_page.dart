221dd04d3adcf45dd994fbef8c1a4a11dd9c586e
NicolasDutronc <nicolas.dutronc@orange.fr>
2020-07-26 18:49:48 +0200

    refact(app bar): added context as an agrument to the appbar

--- lib/users/login/ui/login_page.dart
+++ lib/users/login/ui/login_page.dart
@@ -10,7 +10,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: getAppBar(),
+      appBar: getAppBar(context),
       body: BlocProvider(
         create: (context) {
           return LoginBloc(
