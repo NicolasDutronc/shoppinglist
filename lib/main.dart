959ffc0ad6e8a8b1352621b4de36dbb5cd3cb9c0
NicolasDutronc <nicolas.dutronc@orange.fr>
2020-08-12 17:50:12 +0200

    feat(mode): added a print statement to know if it is release mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
@@ -15,6 +16,7 @@ import 'package:shoplist/users/repository/http.dart';
 import 'package:shoplist/users/repository/repository.dart';
 
 void main() {
+  print('kReleaseMode: $kReleaseMode');
   String backendUrl = '10.0.2.2:8080';
   BlocSupervisor.delegate = SimpleBlocDelegate();
   runApp(
