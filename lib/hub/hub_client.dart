808776995897ad9776637a492615add841caae83
NicolasDutronc <nicolas.dutronc@orange.fr>
2021-04-26 09:23:24 +0200

    added subscribe and unsubscribe to hub client

--- lib/hub/hub_client.dart
+++ lib/hub/hub_client.dart
@@ -7,26 +7,51 @@ class HubClient {
   final String _baseUrl;
   final Dio _dio;
   String _processorId;
-  List<String> _subscribedTopics;
+  Set<String> _subscribedTopics;
 
   HubClient(this._baseUrl, this._dio);
 
-  Stream<HubMessage> Connect() async* {
+  Stream<HubMessage> connect() async* {
     Response<ResponseBody> response;
     response = await _dio.get('$_baseUrl/connect',
         options: Options(responseType: ResponseType.stream));
-    print('connecting...');
+    print('connecting to hub...');
 
-    yield* response.data.stream.map((raw) {
+    yield* response.data.stream.handleError((error, stack) {
+      print('An error occured : $error');
+      print('stack : $stack');
+    }).map((raw) {
       var message = HubMessage.fromJson(jsonDecode(utf8.decode(raw)));
       if (message.type == 'connectionMessage') {
         _processorId = message.msg['processor-id'];
+        return null;
       } else {
         return message;
       }
-    }).handleError((error, stack) {
-      print('An error occured : $error');
-      print('stack : $stack');
-    });
+    }).where((msg) => msg != null);
+  }
+
+  Future<void> subscribe(String topic) async {
+    var response = await _dio.post(
+      '$_baseUrl/subscribe',
+      data: {'topic': topic, 'processor': _processorId},
+      options: Options(responseType: ResponseType.json),
+    );
+
+    if (response.statusCode == 200) {
+      _subscribedTopics.add(topic);
+    }
+  }
+
+  Future<void> unsubscribe(String topic) async {
+    var response = await _dio.post(
+      '$_baseUrl/unsubscribe',
+      data: {'topic': topic, 'processor': _processorId},
+      options: Options(responseType: ResponseType.json),
+    );
+
+    if (response.statusCode == 200) {
+      _subscribedTopics.remove(topic);
+    }
   }
 }
